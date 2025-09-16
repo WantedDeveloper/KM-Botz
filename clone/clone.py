d68d7e02dc080bc81b4cc2393097af4db320ab0b
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-09-16 11:16:56 +0530

    Add files via upload

--- clone/clone.py
+++ clone/clone.py
@@ -231,20 +231,12 @@ async def start(client, message):
                                 updated = True
 
                     elif mode == "request":
-                        if member.status in [
-                            enums.ChatMemberStatus.MEMBER,
-                            enums.ChatMemberStatus.ADMINISTRATOR,
-                            enums.ChatMemberStatus.OWNER,
-                            enums.ChatMemberStatus.RESTRICTED
-                        ]:
-                            if message.from_user.id not in users_counted:
-                                item["joined"] = item.get("joined", 0) + 1
-                                users_counted.append(message.from_user.id)
-                                item["users_counted"] = users_counted
-                                updated = True
-                            continue
-                        else:
-                            buttons.append([InlineKeyboardButton("🔔 Join Channel", url=item["link"])])
+                        if message.from_user.id not in users_counted:
+                            item["joined"] = item.get("joined", 0) + 1
+                            users_counted.append(message.from_user.id)
+                            item["users_counted"] = users_counted
+                            updated = True
+                        continue
 
                 except UserNotParticipant:
                     buttons.append([InlineKeyboardButton("🔔 Join Channel", url=item["link"])])
