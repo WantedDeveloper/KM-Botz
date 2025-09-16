163aaad38883116891b661be9fcfb1250db04e6d
WantedDeveloper <97802491+WantedDeveloper@users.noreply.github.com>
2025-09-16 18:58:40 +0530

    Add files via upload

--- clone/clone.py
+++ clone/clone.py
@@ -239,20 +239,16 @@ async def start(client, message):
                             users_counted.append(message.from_user.id)
                             item["users_counted"] = users_counted
                             updated = True
-                        continue
+                        pass
 
                     except UserNotParticipant:
                         if item.get("link"):
-                            if mode == "request":
-                                buttons.append([InlineKeyboardButton("🔔 Join Channel", url=item["link"])])
-                                if message.from_user.id not in users_counted:
-                                    item["joined"] = joined + 1
-                                    users_counted.append(message.from_user.id)
-                                    item["users_counted"] = users_counted
-                                    updated = True
-                                continue
-                            else:
-                                buttons.append([InlineKeyboardButton("🔔 Join Channel", url=item["link"])])
+                            buttons.append([InlineKeyboardButton("🔔 Join Channel", url=item["link"])])
+                            if message.from_user.id not in users_counted:
+                                item["joined"] = joined + 1
+                                users_counted.append(message.from_user.id)
+                                item["users_counted"] = users_counted
+                                updated = True
 
                     except Exception as e:
                         print(f"⚠️ Error checking member for {ch_id}: {e}")
